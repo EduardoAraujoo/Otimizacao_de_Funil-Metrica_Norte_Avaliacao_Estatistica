d25780b84746ad2fd1d0cb25a315f300e204d0c7
Eduardo Araujo <duduoliveira1911@gmail.com>
2026-03-27 16:32:42 -0300

    Create Pastes

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+# src/utils.py
+import sys
+from src.logger import logging
+from src.exception import CustomException
+
+# Este arquivo fica reservado para funções auxiliares de engenharia de métricas e testes estatísticos,
+# mantendo a lógica de processamento fora do notebook.
